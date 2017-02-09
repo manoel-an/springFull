6e07b58c9dac6e7011acc2a6faa1890d891c9293
Manoel Albino Neto <manoelanagr@gmail.com>
2017-02-09 13:58:09 -0200

    NomeEstiloCadastradoException Handler

--- src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -50,11 +50,7 @@ public class EstilosController {
         if (result.hasErrors()) {
             return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
         }
-        try {
-            estilo = cadastroEstiloService.salvar(estilo);
-        } catch (NomeEstiloCadastradoException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        estilo = cadastroEstiloService.salvar(estilo);
         return ResponseEntity.ok(estilo);
     }
 
