322b28a99c3bae831cd64c41b094b9344fc6f21f
Manoel Albino Neto <manoelanagr@gmail.com>
2017-02-13 13:17:56 -0200

    Página pesquisa cervejas

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -21,6 +21,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.brewer.validation.SKU;
 
@@ -74,6 +75,11 @@ public class Cerveja {
     @ManyToOne
     @JoinColumn(name = "codigo_estilo")
     private Estilo estilo;
+
+    private String foto;
+
+    @Column(name = "content_type")
+    private String contentType;
 
     @PrePersist
     @PreUpdate
@@ -167,6 +173,26 @@ public class Cerveja {
 
     public void setEstilo(Estilo estilo) {
         this.estilo = estilo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public String getFotoOuMock() {
+        return !StringUtils.isEmpty(foto) ? foto : "cerveja-mock.png";
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
     }
 
     @Override
