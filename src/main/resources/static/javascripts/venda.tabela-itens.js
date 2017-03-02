7a3c572ea08a59a70f51dcfe351c1c6a53bc4a1a
Manoel Albino Neto <manoelanagr@gmail.com>
2017-03-02 15:12:08 -0300

    Layout

--- src/main/resources/static/javascripts/venda.tabela-itens.js
+++ src/main/resources/static/javascripts/venda.tabela-itens.js
@@ -5,15 +5,14 @@ Brewer.TabelaItens = (function() {
 		this.tabelaCervejasContainer = $('.js-tabela-cervejas-container');
 		this.uuid = $('#uuid').val();
 		this.emitter = $({});
-		this.on = this.emitter.on.bind(this.emitter);		
+		this.on = this.emitter.on.bind(this.emitter);
 	}
 	
 	TabelaItens.prototype.iniciar = function() {
 		this.autocomplete.on('item-selecionado', onItemSelecionado.bind(this));
-		
 	}
 	
-	function onItemSelecionado(evento, item){
+	function onItemSelecionado(evento, item) {
 		var resposta = $.ajax({
 			url: 'item',
 			method: 'POST',
@@ -22,47 +21,48 @@ Brewer.TabelaItens = (function() {
 				uuid: this.uuid
 			}
 		});
+		
 		resposta.done(onItemAtualizadoNoServidor.bind(this));
 	}
 	
-	function onItemAtualizadoNoServidor(html){
+	function onItemAtualizadoNoServidor(html) {
 		this.tabelaCervejasContainer.html(html);
+		
 		var quantidadeItemInput = $('.js-tabela-cerveja-quantidade-item');
 		quantidadeItemInput.on('change', onQuantidadeItemAlterado.bind(this));
-		quantidadeItemInput.maskMoney({precison: 0, thousands: ''});
-		$('.js-tabela-item').on('dblclick', onDoubleClick);
+		quantidadeItemInput.maskMoney({ precision: 0, thousands: '' });
 		
 		var tabelaItem = $('.js-tabela-item');
-		
-		tabelaItem.on('click', onExclusaoItemClick.bind(this));
+		tabelaItem.on('dblclick', onDoubleClick);
+		$('.js-exclusao-item-btn').on('click', onExclusaoItemClick.bind(this));
 		
 		this.emitter.trigger('tabela-itens-atualizada', tabelaItem.data('valor-total'));
 	}
 	
-	function onQuantidadeItemAlterado(evento){
+	function onQuantidadeItemAlterado(evento) {
 		var input = $(evento.target);
 		var quantidade = input.val();
 		
-		if(quantidade <=0){
+		if (quantidade <= 0) {
 			input.val(1);
 			quantidade = 1;
 		}
-			
+		
 		var codigoCerveja = input.data('codigo-cerveja');
 		
 		var resposta = $.ajax({
 			url: 'item/' + codigoCerveja,
-			method: "PUT",
+			method: 'PUT',
 			data: {
 				quantidade: quantidade,
 				uuid: this.uuid
 			}
 		});
+		
 		resposta.done(onItemAtualizadoNoServidor.bind(this));
 	}
 	
 	function onDoubleClick(evento) {
-		//var item = $(evento.currentTarget); ou
 		$(this).toggleClass('solicitando-exclusao');
 	}
 	
